6512b44842ca2bea44bffae05827be5f0e48433f
Raphael Pholds <raphaelpholds15@gmail.com>
2024-12-16 21:00:06 -0300

    .

---
+++ Dia 3/dia 3.js
@@ -0,0 +1,45 @@
+//let e const
+console.log('Hoje estou aprendendo Variaveis')
+
+console.log('===========Texto===========')
+
+console.log('Tipo texto')
+let nome = 'Raphael Moreira Dias'
+console.log (nome)
+nome = "Raphael Pholds"
+console.log (nome)
+
+console.log ('===========Constante===========')
+
+
+const nomeA = ('Pholds não é meu sobrenome mas sim um nome "artistico"') 
+console.log (nomeA)
+
+console.log ('===========Números===========')
+
+console.log ('Adicionei "let quatro" com o valor 4')
+console.log ('Adiconei "let cinco" com o valor 5')
+console.log ('Em seguida o codigo console.log(quatro + cinco) ')
+console.log ('Resultado em baixo')
+let quatro = 4
+let cinco = 5
+console.log(quatro + cinco)
+
+console.log ('===========Números + Textos===========')
+
+console.log('Usando o let texto + let número a soma dos valores sao diferentes')
+console.log('Por exemplo eu colocar um "let texto" com o valor de 4 e somar com o "let numérico" com o valor 4 veja o que acontece')
+let quatrotexto = ('4')
+console.log (quatrotexto + quatro)
+console.log('Ele somente adiciona como se fosse uma frase de texto')
+
+console.log ('===========Modulos Numéricos===========') 
+
+console.log('Modulos é basicamente o quanto sobra de um numero multiplicado ate chegar em um certo números ')
+console.log('Vamos colocar como base o numero 15, e para prencher o numero eu escolho o numero 4 ')
+console.log ('4 x 3 tem o resultado de 12 mas se multiplicar por 4 daria 16 ou seja ultrapassou o valor base.')
+console.log ('Entao o valor que fica mais aproximado dentro do número base é o escolhido, assim sobrando 3')
+console.log ('Abaixo irei fazer o modulo "15 % 4"')
+let quinze = 15
+console.log (15 % 4)
+console.log ('Normalmente pelo que aprendi ate agora somente numeros impares com numeros pares sobram ')
